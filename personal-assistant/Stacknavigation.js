6b415406934d420a85547eb3f24b7d4527540f6c
Ivan Arcos Garcia <91813117+ivanArco@users.noreply.github.com>
2025-03-28 12:29:35 -0600

    Implementacion de Interterfaz

--- personal-assistant/Stacknavigation.js
+++ personal-assistant/Stacknavigation.js
@@ -4,6 +4,11 @@ import RegistroScreen from './paginas/Registrarse';
 import IniciarSesionScreen from './paginas/IniciarSesion';
 import HomeScreen from './AppScreen';
 import Inicio from './paginas/Inicio';
+import AgendaScreen from './paginas/Agenda';
+import PerfilScreen from './paginas/Perfil';
+import HistorialScreen from './paginas/Historial';
+import SearchScreen from './paginas/Busquedas';
+;
 
 const Stack = createStackNavigator();
 
@@ -14,7 +19,11 @@ const Stacknavigator = () => {
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="IniciarSesion" component={IniciarSesionScreen} />
         <Stack.Screen name="Registro" component={RegistroScreen} />
-        <Stack.Screen name="Inicio" component={Inicio}/>
+        <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="Agenda" component={AgendaScreen} />
+        <Stack.Screen name="Historial" component={HistorialScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+        <Stack.Screen name="Busquedas" component={SearchScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
